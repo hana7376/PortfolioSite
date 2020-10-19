45b25ba5b7f563608bd82aec05df8d4196a9b6d1
Hana <71799213+hana7376@users.noreply.github.com>
2020-10-19 14:28:28 -0400

    added buttons

---
+++ scripts/buttons.js
@@ -0,0 +1,8 @@
+function sayGoodbye() {
+    alert("Bye, come back soon");
+}
+
+function greeting() {
+   let name = prompt("What's your name?");
+   alert("hello, welcome to my site, " + name )
+}
