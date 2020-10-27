9c01c0c5da1a605dd26eaaee5d589d8d08a46eeb
Hana <71799213+hana7376@users.noreply.github.com>
2020-10-27 09:20:51 -0400

    assignment 4 work

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -6,3 +6,43 @@ function greeting() {
    let name = prompt("What's your name?");
    alert("hello, welcome to my site, " + name )
 }
+
+function Button1() {
+    alert("Function 1 coming soon");
+}
+
+function Button2() {
+    alert("Function 2 comming soon");
+}
+
+function Button3() {
+    alert("Function 3 coming soon");
+}
+
+function Button4() {
+    alert("Function 4 coming soon");
+}
+
+function Button5() {
+    alert("Function 5 coming soon");
+}
+
+function Button6() {
+    alert("Function 6 coming soon");
+}
+
+function Button7() {
+    alert("Function 7 coming soon");
+}
+
+function Button8() {
+    alert("Function 8 coming soon");
+}
+
+function Button9() {
+    alert("Function 9 comming soon");
+}
+
+function Button10() {
+    alert("Function 10 coming soons");
+}
